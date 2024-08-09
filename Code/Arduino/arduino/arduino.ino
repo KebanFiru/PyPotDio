4a7fe085e3a99d72be52d2843f7776240fbb52ff
Hakan <143508175+KebanFiru@users.noreply.github.com>
2024-08-09 16:04:16 +0300

    Now code only send data when value of pot change

--- Code/Arduino/arduino/arduino.ino
+++ Code/Arduino/arduino/arduino.ino
@@ -1,9 +1,27 @@
+int Data[2];
+int mainData[3];
+
 void setup() {
   Serial.begin(9600);
+
 }
 
 void loop() {
   int data = analogRead(A3); //This is the pin connect to middle terminal of pot
-  int newData = map(data, 0,1023,0,100); 
-  Serial.println(newData);
-}
+  int newData = map(data, 0,1020,0,100); 
+ for (int i = 0; i < 3; i++) {
+     mainData[i] = newData;
+     if (i == 3) {
+      i = 0;
+    }
+    Data[0] = mainData[1];
+    Data[1] = mainData[2];
+    if(Data[0] < Data[1]){
+      Serial.println(newData); }
+    else if (Data[0] > Data[1]){
+      Serial.println(newData);
+  }
+  
+  
+}}
+  
