a8b2d2fefa93ccfe43235336ea11e245df00c3fe
Hakan <143508175+KebanFiru@users.noreply.github.com>
2024-08-11 14:36:24 +0300

    Flickering issue fixed and code readibilty increase

--- Code/Arduino/arduino/arduino.ino
+++ Code/Arduino/arduino/arduino.ino
@@ -1,27 +1,35 @@
-int Data[2];
+ int Data[2];
 int mainData[3];
 
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
   int data = analogRead(A3); //This is the pin connect to middle terminal of pot
-  int newData = map(data, 0,1020,0,50); 
- for (int i = 0; i < 3; i++) {
-     mainData[i] = newData;
-     if (i == 3) {
-      i = 0;
-    }
-    Data[0] = mainData[1];
+
+  int newData = map(data, 0,1020,0,50); //Maping pot data from 0 to 100
+
+  delay(10);
+ for (int i = 0; i < 3; i++) { // for loop to add mapped AnalogRead(A3) to mainData array
+
+    mainData[i] = newData;
+
+    Data[0] = mainData[1]; 
+
     Data[1] = mainData[2];
-    if(Data[0] < Data[1]){
-      Serial.println(newData); }
-    else if (Data[0] > Data[1]){
-      Serial.println(newData);
-  }
+
+    if (Data[0] > Data[1]){
+      Serial.println(newData);}
+    else if (Data[0] < Data[1]){
+      Serial.println(newData);}
+
+    }  //For loop 
+     
+
+} //Void loop
+
   
-  
-}}
+              
+
   
